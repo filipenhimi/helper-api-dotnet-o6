3f0e88940e08e961838f4c07ca63fe24c86073e8
unknown <jpluciodev@gmail.com>
2024-09-05 17:57:03 -0300

    feat: adicao unit test e tratamento de erro

--- helper-api-dotnet-o6/Controllers/CnpjController.cs
+++ helper-api-dotnet-o6/Controllers/CnpjController.cs
@@ -1,11 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using helper_api_dotnet_o6.Models;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using helper_api_dotnet_o6.Models.Cep;
 using helper_api_dotnet_o6.Models.Cnpj;
+using helper_api_dotnet_o6.Models;
+using System.Diagnostics;
 
 namespace helper_api_dotnet_o6.Controllers
 {
@@ -15,44 +17,46 @@ namespace helper_api_dotnet_o6.Controllers
     public class CnpjController : ControllerBase
     {
         private readonly string _endPoint = "https://open.cnpja.com/office";
+        private const int Status429LimiteMinutiExcedido = 429;
 
         [HttpGet]
         [Route("/cnpj/completo/{cnpj}")]
-        [ProducesResponseType(typeof(CnpjResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(CnpjDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status502BadGateway)]
         [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
         public async Task<IActionResult> ConsultaCnpj(string cnpj)
         {
-            try
+           try
             {
                 CnpjResponse cnpjResponse = await GetCnpjAsync(cnpj);
 
                 if (cnpjResponse == null)
                 {
-                    return NoContent();
+                    return NotFound("CNPJ não encontrado.");
                 }
 
-                return Ok(cnpjResponse);
+                return Ok(converterModelToDTO(cnpjResponse));
+            }
+            catch (HttpRequestException ex) 
+            {
+                return BadRequest(ex.Message);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex); 
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno no servidor.");
+               return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno no servidor.");
             }
         }
 
         [HttpGet]
         [Route("/cnpj/resumo/{cnpj}")]
-        [ProducesResponseType(typeof(CnpjResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(CnpjDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status502BadGateway)]
         [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
@@ -64,24 +68,45 @@ namespace helper_api_dotnet_o6.Controllers
 
                 if (cnpjResponse == null)
                 {
-                    return NoContent();
+                    return NotFound("CNPJ não encontrado.");
                 }
 
                 return Ok(converterModelToDTO(cnpjResponse));
             }
+            catch (HttpRequestException ex) 
+            {
+                return BadRequest(ex.Message);
+            }
             catch (Exception ex)
             {
-                Console.WriteLine(ex); 
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno no servidor.");
+               return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno no servidor.");
             }
         }
 
-        private async Task<CnpjResponse> GetCnpjAsync(string cnpj)
+        private async Task<Models.CnpjResponse> GetCnpjAsync(string cnpj)
         {
             using (var httpClient = new HttpClient())
             {
-                var response = await httpClient.GetStringAsync($"{_endPoint}/{cnpj}");
-                return JsonSerializer.Deserialize<CnpjResponse>(response);
+                var response = await httpClient.GetAsync($"{_endPoint}/{cnpj}");
+
+                if (response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    throw new HttpRequestException("Parâmetro de consulta mal formatado ou faltante.");
+                }
+
+                else if (response.StatusCode == HttpStatusCode.TooManyRequests)
+                {
+                    throw new HttpRequestException("Créditos esgotados ou limite por minuto excedido.");
+                }
+                
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new HttpRequestException("CNPJ não registrado na Receita Federal.");
+
+                }
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<CnpjResponse>(responseString);
             }
         }
         
@@ -95,6 +120,11 @@ namespace helper_api_dotnet_o6.Controllers
            
             return cnpjDto;
 
+        }
+
+        internal async Task ConsultaCnpjDto()
+        {
+            throw new NotImplementedException();
         }
     }
     
