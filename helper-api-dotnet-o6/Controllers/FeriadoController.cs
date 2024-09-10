318801e9ccc3e15c76b4b062339d6f534b28d44d
Leonardo <leo.ronchini@hotmail.com>
2024-09-09 23:06:19 -0300

    Create feriado controller and model

---
+++ helper-api-dotnet-o6/Controllers/FeriadoController.cs
@@ -0,0 +1,50 @@
+﻿using helper_api_dotnet_o6.Models.Feriado;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace helper_api_dotnet_o6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeriadoController : ControllerBase
+    {
+        private readonly string _endPoint = "https://brasilapi.com.br/api/feriados/v1/";
+        private readonly ILogger<FeriadoController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public FeriadoController(ILogger<FeriadoController> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        [Route("{year}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(int year)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"{_endPoint}{year}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var feriadosData = await response.Content.ReadAsStringAsync();
+                    var feriadosModel = JsonConvert.DeserializeObject<List<Feriado>>(feriadosData);
+                    return Ok(feriadosModel);
+                }
+
+                return BadRequest("Ano inválido ou dados não encontrados.");
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "Erro ao consultar os feriados.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao consultar os feriados.");
+            }
+        }
+    }
+}
---
+++ helper-api-dotnet-o6/Models/Feriado/FeriadoModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace helper_api_dotnet_o6.Models.Feriado
+{
+    public class Feriado
+    {
+        [JsonProperty("date")]
+        public string Data { get; set; }
+
+        [JsonProperty("name")]
+        public string Nome { get; set; }
+
+        [JsonProperty("type")]
+        public string Tipo { get; set; }
+    }
+}
